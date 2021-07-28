1969cc959f0bd32ebaa3e11d71da71e5812a1160
GL-19 <loiolagl19@gmail.com>
2021-07-28 20:12:08 -0300

    Jogo funcionando

--- js/script.js
+++ js/script.js
@@ -1,14 +1,19 @@
 let canvas = document.getElementById("snake");
 let context = canvas.getContext("2d");
+
+//boxSize * numberOfBoxes tem que ser igual ao width e ao height do canvas
 let boxSize = 32;
 let numberOfBoxes = 16;
 let snake = [];
 snake[0] = {
-    x: 8 * boxSize,
-    y: 8 * boxSize
+    x: Math.floor(0.5 * numberOfBoxes * boxSize),
+    y: Math.floor(0.5 * numberOfBoxes * boxSize) 
 };
-
 let direction = "right";
+let food = {
+    x: Math.floor(Math.random() * numberOfBoxes) * boxSize,
+    y: Math.floor(Math.random() * numberOfBoxes) * boxSize
+};
 
 function createBackGround() {
     context.fillStyle = "lightblue";
@@ -22,6 +27,11 @@ function createSnake() {
     }
 }
 
+function drawFood() {
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, boxSize, boxSize);
+}
+
 document.addEventListener('keydown', event => {        
     if(event.keyCode === 37 && direction !== "right") direction = "left";
     if(event.keyCode === 38 && direction !== "down") direction = "up";
@@ -29,14 +39,21 @@ document.addEventListener('keydown', event => {
     if(event.keyCode === 40 && direction !== "up") direction = "down";
 })
 
-function startGame() {
+function renderGame() {
     if(snake[0].x > (numberOfBoxes - 1) * boxSize && direction === "right") snake[0].x = 0;
     if(snake[0].x < 0 && direction === "left") snake[0].x = numberOfBoxes * boxSize;
     if(snake[0].y > (numberOfBoxes - 1) * boxSize && direction === "down") snake[0].y = 0;
     if(snake[0].y < 0 * boxSize && direction === "up") snake[0].y = numberOfBoxes * boxSize;
 
+    for(i = 1; i < snake.length; i++) {
+        if(snake[0].x === snake[i].x && snake[0].y === snake[i].y) {
+            clearInterval(game);
+            alert("Game Over");
+        }
+    }
     createBackGround();
     createSnake();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
@@ -46,8 +63,12 @@ function startGame() {
     if(direction === "up") snakeY -= boxSize;
     if(direction === "down") snakeY += boxSize;
 
-    snake.pop();
-
+    if(snakeX !== food.x || snakeY !== food.y) {
+        snake.pop();
+    } else  {
+        food.x = Math.floor(Math.random() * numberOfBoxes) * boxSize;
+        food.y = Math.floor(Math.random() * numberOfBoxes) * boxSize;
+    }
     let newHead = {
         x: snakeX,
         y: snakeY
@@ -55,4 +76,5 @@ function startGame() {
     snake.unshift(newHead);
 }
 
-let game = setInterval(startGame, 100);
+let game = setInterval(renderGame, 100);
+
