5f72dfa7740677ffe5e3239c8d90571381eb8d19
GL-19 <loiolagl19@gmail.com>
2021-07-28 17:49:16 -0300

    Adicionado movimento da cobra

--- js/script.js
+++ js/script.js
@@ -7,13 +7,40 @@ snake[0] = {
     y: 8 * box
 };
 
+let direction = "right";
+
 function createBackGround() {
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "lightblue";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 function createSnake() {
-
+    for(i = 0; i < snake.length; i++) {
+        context.fillStyle = "blue";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
 }
 
-createBackGround();
+function startGame() {
+    createBackGround();
+    createSnake();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction === "right") snakeX += box;
+    if(direction === "left") snakeX -= box;
+    if(direction === "up") snakeY -= box;
+    if(direction === "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+    snake.unshift(newHead);
+}
+
+
+let game = setInterval(startGame, 100);
