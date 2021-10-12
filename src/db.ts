3ed46d7e01ad33cf9dc39b8e14edead3fd19795f
GL-19 <loiolagl19@gmail.com>
2021-10-11 21:57:38 -0300

    Configuração e conexão com db implementadas

---
+++ src/db.ts
@@ -0,0 +1,9 @@
+import { Pool } from "pg";
+import DB_URL from "./env";
+
+const connectionString = DB_URL;
+//const connectionString = process.env.DB_URL;
+console.log(connectionString);
+const db = new Pool({ connectionString });
+
+export default db;
